557a4394fe43fec88cae195c5c8bf5958623fdee
lauwdude@hotmail.com <1TE8zmOfVOJJx16160u2>
2015-06-20 12:17:39 +0200

    Logging

--- src/main/java/Messages/RequestHandler.java
+++ src/main/java/Messages/RequestHandler.java
@@ -1,11 +1,14 @@
 package Messages;
 
+import java.util.Arrays;
+
 /**
  * Created by Laurence on 20/6/2015.
  */
 public class RequestHandler {
 
     public String handleMessage(String request, String[] commands, String channel) {
+        System.out.println(request + " on channel " + channel + " commands: " + Arrays.toString(commands));
         return "OKE!";
     }
 }
